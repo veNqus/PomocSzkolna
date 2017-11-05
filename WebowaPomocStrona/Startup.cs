2ccf32b9578eaf2e79fd4c8444c972066564cdd0
veNqus <Azragot@gmail.com>
2017-11-05 13:52:12 +0100

    Add project files.

---
+++ WebowaPomocStrona/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebowaPomocStrona.Startup))]
+namespace WebowaPomocStrona
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
